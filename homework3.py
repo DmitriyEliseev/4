b15bbd05e88fc54b0632db35852fb325df0a4a0c
DmitriyEliseev <93958489+DmitriyEliseev@users.noreply.github.com>
2024-07-24 11:08:12 +0300

    Update homework3.py так верно?

--- homework3.py
+++ homework3.py
@@ -2,7 +2,6 @@ name = 'Dima'
 print (name)
 age = 31
 print (age)
-age_new = 32
-print (age_new)
-is_student = 'is_student' and (31<32)
-print('is_student', 31<32)
+age+=1
+print (age)
+print('is_student:',31<32)
